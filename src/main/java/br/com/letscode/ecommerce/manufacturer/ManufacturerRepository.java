823f229d25918f05a143e50e86249a90a8935111
Guilherme Vaccariello <76708977+GVJM@users.noreply.github.com>
2022-04-07 18:38:21 -0300

    Delete src/main/java/br/com/letscode/ecommerce directory

--- src/main/java/br/com/letscode/ecommerce/manufacturer/ManufacturerRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.letscode.shop.manufacturer;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ManufacturerRepository extends JpaRepository<ManufacturerEntity, Long> {
-}
--- src/main/java/br/com/letscode/ecommerce/product/ProductStatus.java
+++
@@ -1,5 +0,0 @@
-package br.com.letscode.shop.product;
-
-public enum ProductStatus {
-    ACTIVE,INACTIVE;
-}
